fff39296e3df44b0b5ed50aa69637e3f7122cd87
CharlyssaG <cgulledge@kidkare.com>
2026-04-23 10:07:00 -0400

    Update client.ts

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,7 +1,7 @@
 'use client';
 
 import { createBrowserClient } from '@supabase/ssr';
-import type { Database } from './database.types';
+import type { Database } from '../database.types';
 
 export function createClient() {
   return createBrowserClient<Database>(
